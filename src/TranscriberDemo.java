5e44b28f9eda154cde2832f677bf606a2e55e9d5
ayarmark <58202564+ayarmark@users.noreply.github.com>
2021-07-21 11:59:57 -0400

    adding code to work with microphone

--- src/TranscriberDemo.java
+++ src/TranscriberDemo.java
@@ -1,31 +1,32 @@
 
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
 import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 import edu.cmu.sphinx.api.SpeechResult;
 import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 
-public class TranscriberDemo {       
+public class TranscriberDemo {
 
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        Configuration configuration = new Configuration();
+		Configuration configuration = new Configuration();
 
-        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
-        configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
-        configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+		configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+		configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+		configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
-	StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
-	InputStream stream = new FileInputStream(new File("test.wav"));
+		LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+		// Start recognition process pruning previously cached data.
+		recognizer.startRecognition(true);
+		System.out.println("Start talking now.");
+		SpeechResult result = recognizer.getResult();
+		// Pause recognition process. It can be resumed then with startRecognition(false).
+		System.out.println("Stop talking now.");
+		recognizer.stopRecognition();
 
-        recognizer.startRecognition(stream);
-	SpeechResult result;
-        while ((result = recognizer.getResult()) != null) {
-	    System.out.format("Hypothesis: %s\n", result.getHypothesis());
+			System.out.format("Hypothesis: %s\n", result.getHypothesis());
 	}
-	recognizer.stopRecognition();
-    }
 }
